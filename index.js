597bc07d4545689fd050dfbeec466509091febdc
Ridho Ahmad Batubara <ridho.beteer@gmail.com>
2023-03-02 02:34:28 +0700

    add morgan and cors

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
 const routerNavigation = require("./src/routers");
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 8080;
 
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
