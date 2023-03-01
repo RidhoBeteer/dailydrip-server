b2f01ef3691413407c4c0a1ee31f097d9851ad46
Ridho Ahmad Batubara <ridho.beteer@gmail.com>
2023-03-01 14:52:45 +0700

    create GET handler

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Daily Drip API",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
