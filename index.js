8073a4ed6cf10b73647101830575a8e690f5917a
Ridho Ahmad Batubara <ridho.beteer@gmail.com>
2023-03-01 14:15:08 +0700

    create basic server

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
